08a277b2afb90d43ce6d853e7ecf75f57315a3d9
Jenifa <jenifa.joseph@georgebrown.ca>
2026-09-16 23:26:24 -0400

    Completed Lab 1 Hello Android exercise

---
+++ app/src/main/java/ca/gbc/comp3074/joseph/lab1helloandroid/MainActivity.kt
@@ -0,0 +1,35 @@
+package ca.gbc.comp3074.joseph.lab1helloandroid
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val message = findViewById<TextView>(R.id.textViewMessage)
+        val btn = findViewById<Button>(R.id.button)
+
+        btn.setOnClickListener {
+            message.text = getString(R.string.button_clicked)
+
+            btn.setBackgroundColor(getColor(R.color.green))
+
+
+        }
+    }
+}
